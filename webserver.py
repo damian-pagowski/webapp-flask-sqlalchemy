ddc06f97123be5e05fe5cc8129cc30c5bab8951e
Damian <d.pagowski@gmail.com>
2019-09-25 19:14:16 +0700

    added routes and template for menu

--- webserver.py
+++ webserver.py
@@ -1,21 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 from db_server import create_session
 from database_setup import Restaurant, MenuItem
 
 session = create_session()
 app = Flask(__name__)
 
+
+@app.route('/restaurants/<int:restaurant_id>/new')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
+
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    restaurant = session.query(Restaurant).first()
+@app.route('/restaurants/<int:param_restaurant_id>')
+def restaurants(param_restaurant_id = 0):
+    restaurant = None 
+    if param_restaurant_id is not 0:
+        restaurant = session.query(Restaurant).filter_by(id=param_restaurant_id).one()
+    else:
+        restaurant = session.query(Restaurant).first()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
-    output = '<h1>%s</h1><h5>Menu:</h5><ul>' % restaurant.name
-    for i in items:
-        row = '<li>%s</li>' % i.name
-        output += row
-    output += "</ul>"
-    return output 
+    return render_template('menu.html', restaurant=restaurant, items=items)
 
 
 if __name__ == '__main__':
